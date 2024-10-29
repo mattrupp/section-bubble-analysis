0ed1aa36ec33ce0c1be208f7649dfcfbcaec022c
Matt Rüpp <mattrupp@gmail.com>
2024-10-29 09:41:20 -0400

    filter usecols that don't exist

--- xlsx_to_csv.py
+++ xlsx_to_csv.py
@@ -2,10 +2,18 @@ import os
 import pandas as pd
 from pathlib import Path
 
+
 # combine all sheets from an excel file
 def read_and_concat_sheets(file_path, selected_columns):
+
     # Read the Excel file into an ExcelFile object
     excel_file = pd.ExcelFile(file_path)
+    # Read the header row to get the existing column names
+    df_header = pd.read_excel(file_path, nrows=0)
+    existing_cols = df_header.columns.tolist()
+    
+    if selected_columns:
+        selected_columns = [col for col in selected_columns if col in existing_cols]
 
     # Create an empty list to store the DataFrames
     dfs = []
@@ -37,6 +45,13 @@ def xls_to_csv(file_path, selected_columns=[], overwrite=False, multiple_sheets=
                     df = read_and_concat_sheets(file_path, selected_columns)
                 else:
                     excel_file = pd.ExcelFile(file_path)
+                    # Read the header row to get the existing column names
+                    df_header = pd.read_excel(file_path, nrows=0)
+                    existing_cols = df_header.columns.tolist()
+                    
+                    if selected_columns:
+                        selected_columns = [col for col in selected_columns if col in existing_cols]
+
                     df = excel_file.parse(usecols=selected_columns)
                 # Write the DataFrame to a CSV file
                 df.to_csv(csv_file_path, index=False)
